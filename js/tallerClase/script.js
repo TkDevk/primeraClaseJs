c805a4be615a4a51d92ce3922f9ec23bdbddf4f1
TkDevk <duvanuriel@hotmail.com>
2023-09-12 17:31:01 -0500

    funciones y peticiones por consola

--- js/tallerClase/script.js
+++ js/tallerClase/script.js
@@ -1,21 +1,28 @@
-let num1 = parseFloat(document.getElementById("num1").value);
-let num2 = parseFloat(document.getElementById("num2").value);
+//El error estaba en que cuando click la variable se va a recargar entonces si son globales no se van a recargar con el valor de la funcion sino globalmente por lo que no pueden recoger el valor de funcion
 
+function sumar() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let resultado = num1 + num2; //primero donde va acumular
+    document.getElementById("resultado").textContent = resultado;
+}
+function restar() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let resultado = num1 - num2; //primero donde va acumular
+    document.getElementById("resultado").textContent = resultado;
+}
+function multiplicar() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let resultado = num1 * num2; //primero donde va acumular
+    document.getElementById("resultado").textContent = resultado;
+}
+function dividir() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let resultado = num1 / num2; //primero donde va acumular
+    document.getElementById("resultado").textContent = resultado;
+}
 
-
-function sumar(){
-   resultado=num1+num2; //primero donde va acumular
-   let resultado = document.getElementById("resultado").textContent;
-}
-function restar(){
-    resultado=num1-num2; //primero donde va acumular
-   let resultado = document.getElementById("resultado").textContent;
-}
-function multiplicar(){
-    resultado=num1*num2; //primero donde va acumular
-   let resultado = document.getElementById("resultado").textContent;
-}
-function dividir(){
-    resultado=num1/num2; //primero donde va acumular
-   let resultado = document.getElementById("resultado").textContent;
-}
+//shift+alt+f
